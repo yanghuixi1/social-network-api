26b7332fcee01a7e45d0cd7086267de77bb4a634
yanghuixi1 <yanghui.xi1@gmail.com>
2022-01-30 23:14:38 -0500

    Delete date from all collections at startup

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,8 @@ const { connect, connection } = require("mongoose");
 connect("mongodb://localhost/socialNetwork", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).then((connection) =>
+  Object.values(connection.models).forEach((model) => model.deleteMany({}))
+);
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
-
 const PORT = 3001;
 const app = express();
 
